cbeb2ccdeced06158a65a75d770b45eb35f8b459
Hussain <113285362+HussainMayoof@users.noreply.github.com>
2026-08-18 16:07:54 +0300

    Added dropdown menu for user

--- former-frontend/src/components/Header/index.tsx
+++ former-frontend/src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router';
 import UnderlinedLink from '../shared/UnderlinedLink.tsx';
 import { useUser, useUserActions } from '../../store.ts';
 import ThemeToggle from './ThemeToggle.tsx';
+import { BsBoxArrowRight } from 'react-icons/bs';
 
 const Header = () => {
     const user = useUser();
@@ -26,15 +27,34 @@ const Header = () => {
                             New Post
                         </UnderlinedLink>
 
-                        <div
-                            className="avatar cursor-pointer avatar-placeholder"
-                            onClick={logout}
-                        >
-                            <div className="bg-neutral text-neutral-content w-8 rounded-full">
-                                <span className="text-md">
-                                    {user.username[0].toUpperCase()}
-                                </span>
+                        <div className="dropdown">
+                            <div
+                                className="avatar cursor-pointer avatar-placeholder"
+                                tabIndex={0}
+                                role="button"
+                            >
+                                <div className="bg-neutral text-neutral-content w-8 rounded-full">
+                                    <span className="text-md">
+                                        {user.username[0].toUpperCase()}
+                                    </span>
+                                </div>
                             </div>
+
+                            <ul
+                                tabIndex={-1}
+                                className="dropdown-content menu bg-base-200 rounded-box z-1 w-40 mt-0.5 shadow-xl right-0 duration-200"
+                            >
+                                <li>
+                                    <a href={`/users/${user.username}`}>
+                                        View Profile
+                                    </a>
+                                </li>
+                                <li onClick={logout}>
+                                    <p className="text-error">
+                                        Log Out <BsBoxArrowRight />
+                                    </p>
+                                </li>
+                            </ul>
                         </div>
                     </>
                 )}
